e12aacb726de73bdc8ae51e77240f06bfdaee8fc
isaaclal124 <isaaclal124@gmail.com>
2022-08-25 15:18:13 -0400

    changed comments

---
+++ Homework/HW-E1.7.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Isaac Lal
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Homework E1.7
+
+Write a program that prints three items, such as the names of 
+your three best friends or favorite movies, on three separate lines.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Best "
+}
--- Labs/Lab 1/smaller3.cpp
+++ Labs/Lab 1/smaller3.cpp
@@ -4,7 +4,8 @@ Course: CSCI-135
 Instructor: Genady Maryash
 Assignment: Lab 1B
 
-Write a program smaller3.cpp that asks the user to input three integer numbers, and prints out the smallest of the three.
+Write a program smaller3.cpp that asks the user to input three integer numbers, 
+and prints out the smallest of the three.
 */
 
 #include <iostream>
@@ -13,6 +14,7 @@ using namespace std;
 int main() {
     int num1, num2, num3;
 
+    // This sequence asks the user to input three numbers
     cout << "Enter the first number: " << endl;
     cin >> num1;
     cout << "Enter the second number: " << endl;
@@ -20,6 +22,7 @@ int main() {
     cout << "Enter the third number: " << endl;
     cin >> num3;
 
+    // These if-statements calculate the smaller of the three numbers inputted
     if(num1 < num2 && num2 < num3) {
         cout << "The smaller of the three is " << num1 << endl;
     } else if(num2 < num1 && num2 < num3) {
