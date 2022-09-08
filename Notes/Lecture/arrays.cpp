7ae57e096e8e2e4387c126858a55bd64250c33e8
isaaclal124 <isaaclal124@gmail.com>
2022-09-08 09:19:20 -0400

    changed name of remote + arrays notes

---
+++ Notes/Lecture/arrays.cpp
@@ -0,0 +1,10 @@
+// Lecture 9/8: Array Elements
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double values[10];      // array notation: it will have 10 values 
+    values[4] = 3;      // initialize the value
+    cout << values[4] << endl;      // output the result 
+}
