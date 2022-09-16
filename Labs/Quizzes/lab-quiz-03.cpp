6eaec899c7cda0c692069ee0b7e7321b03f7790e
isaaclal124 <isaaclal124@gmail.com>
2022-09-16 00:10:08 -0400

    lab quiz 3 update

--- Labs/Quizzes/lab-quiz-03.cpp
+++ Labs/Quizzes/lab-quiz-03.cpp
@@ -18,8 +18,8 @@ using namespace std;
 int main() {
     int arr[10];
 
-    for(int i = 1; i <= 10; i++) {
-        arr[i] = i;
+    for(int i = 0; i < 10; i++) {
+        arr[i] = i + 1;
         cout << arr[i] << endl;
     }
     
